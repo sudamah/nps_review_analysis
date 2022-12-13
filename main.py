0a945730b9a6c34b87b6b44a6b85601fafba2b18
sudamasharma <sudama.sharma@heptagon.in>
2022-12-13 11:57:31 +0530

    changes

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 # importing  all the
 
 from modules.services import LoadAndCleanData, TopicClassifier, SentimentClassifier, TypeClassifier
-from modules.utils import LabelsToDataframe
+# from modules.utils import LabelsToDataframe
 
-path = '/home/heptagon/Desktop/nps_analysis/reviews_data.csv'
+path = '/home/heptagon/Desktop/nps_review_analysis/movie_reviews.csv'
 
 candidate_labels = ['appreciation', 'information', 'complaint']
 
 load_data = LoadAndCleanData(path)
 corpus_list = TopicClassifier(load_data.clean_text_and_lemmatize())
-sentiments_list = SentimentClassifier(load_data.clean_text_and_lemmatize())
-type_list = TypeClassifier(load_data.convert_empty_string_to_none())
+# sentiments_list = SentimentClassifier(load_data.clean_text_and_lemmatize())
+# type_list = TypeClassifier(load_data.convert_empty_string_to_none())
 
-data_frame = LabelsToDataframe()
+# data_frame = LabelsToDataframe(path)
 
 if __name__ == '__main__':
 
     # calling functions
 
+    # data_frame.clean_text_and_lemmatize()
+
     load_data.clean_text_and_lemmatize()
     corpus_list.get_keywords_phreses()
-    sentiments_list.get_sentiment_classifier_model()
-    type_list.get_type_classifier_model(candidate_labels)
+    # sentiments_list.get_sentiment_classifier_model()
+    # type_list.get_type_classifier_model(candidate_labels)
--- modules/utils.py
+++ modules/utils.py
@@ -1,16 +1,24 @@
-import pandas as pd
+# import pandas as pd
 
-from modules.services import LoadAndCleanData, TopicClassifier, SentimentClassifier, TypeClassifier
+# from modules.services import LoadAndCleanData, TopicClassifier, SentimentClassifier, TypeClassifier
 
 
-class LabelsToDataframe:
+# class LabelsToDataframe:
 
-    def __init__(self, data):
-        self.data = data
+#     def __init__(self, path):
+#         self.path = path            # Get the File Path from the directory
 
-    def get_data_frame(self):
+#     def get_data_frame(self):
 
-        path = '/home/heptagon/Desktop/nps_analysis/reviews_data.csv'
-        load_data = LoadAndCleanData(path)
+#         candidate_labels = ['appreciation', 'information', 'complaint']
 
-        load_data.clean_text_and_lemmatize()
+#         load_data = LoadAndCleanData()
+#         corpus_list = TopicClassifier(load_data.clean_text_and_lemmatize())
+#         sentiments_list = SentimentClassifier(
+#             load_data.clean_text_and_lemmatize())
+#         type_list = TypeClassifier(load_data.convert_empty_string_to_none())
+
+#         load_data.clean_text_and_lemmatize()
+#         corpus_list.get_keywords_phreses()
+#         sentiments_list.get_sentiment_classifier_model()
+#         type_list.get_type_classifier_model(candidate_labels)
